e610238b3507c26dac517d6036ec672de5a9745b
Grazielle <grazielle.anaia@gmail.com>
2025-05-30 11:16:43 -0400

    Performed unit test on ComunicacaoUpdateMapperTest class

--- src/main/java/com/luizalebs/comunicacao_api/business/converter/ComunicacaoUpdateMapper.java
+++ src/main/java/com/luizalebs/comunicacao_api/business/converter/ComunicacaoUpdateMapper.java
@@ -6,8 +6,12 @@ import com.luizalebs.comunicacao_api.infraestructure.entities.ComunicacaoEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+
 public interface ComunicacaoUpdateMapper {
+
+    ComunicacaoUpdateMapper INSTANCE = Mappers.getMapper(ComunicacaoUpdateMapper.class);
     ComunicacaoEntity updateComunicacao(ComunicacaoInDTO comunicacaoInDTO, @MappingTarget ComunicacaoEntity entity);
 }
--- target/generated-sources/annotations/com/luizalebs/comunicacao_api/business/converter/ComunicacaoUpdateMapperImpl.java
+++ target/generated-sources/annotations/com/luizalebs/comunicacao_api/business/converter/ComunicacaoUpdateMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-29T15:35:16-0400",
+    date = "2025-05-30T10:55:14-0400",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
